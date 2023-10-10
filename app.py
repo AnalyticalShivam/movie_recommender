a5a8e2a40a3ece7e20a447f232e7f16cc40041ae
AnalyticalShivam <ssg98670@gmail.com>
2023-10-10 16:04:00 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,55 @@
+import streamlit as st
+import pickle
+import pandas as pd
+import requests
+
+def fetch_poster(movie_id):
+    url = "https://api.themoviedb.org/3/movie/{}?api_key=2c30958663c5f767708d4e64fa6ee2d8&language=en-US".format(
+        movie_id)
+    data = requests.get(url)
+    data = data.json()
+    poster_path = data['poster_path']
+    full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
+    return full_path
+
+
+def recommend(movie):
+    movie_index = movies[movies['title'] == movie].index[0]
+    distances = similarity[movie_index]
+    movies_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[0:5]
+    recommened_movies = []
+    recommended_movies_poster = []
+
+    for i in movies_list:
+        movie_id = movies.iloc[i[0]].movie_id
+        recommened_movies.append(movies.iloc[i[0]].title)
+        recommended_movies_poster.append(fetch_poster(movie_id))
+    return recommened_movies, recommended_movies_poster
+
+
+movies_dict = pickle.load(open('movie_dict', 'rb'))
+movies = pd.DataFrame(movies_dict)
+similarity = pickle.load(open('similarity', 'rb'))
+st.title('Movie Recommender System')
+selected_movies = st.selectbox(
+    'Movies on your demand',
+    movies['title'].values)
+if st.button('recommend'):
+    recommended_movie_names, recommended_movie_posters = recommend(selected_movies)
+    col1, col2, col3, col4, col5 = st.columns(5)
+    with col1:
+        st.text(recommended_movie_names[0])
+        st.image(recommended_movie_posters[0])
+    with col2:
+        st.text(recommended_movie_names[1])
+        st.image(recommended_movie_posters[1])
+
+    with col3:
+        st.text(recommended_movie_names[2])
+        st.image(recommended_movie_posters[2])
+    with col4:
+        st.text(recommended_movie_names[3])
+        st.image(recommended_movie_posters[3])
+    with col5:
+        st.text(recommended_movie_names[4])
+        st.image(recommended_movie_posters[4])
