8bdf35ffea2a075bf652b3d70eb003c33f2e5bf2
AnalyticalShivam <ssg98670@gmail.com>
2023-10-19 17:44:23 +0530

    exception handling

--- app.py
+++ app.py
@@ -34,22 +34,26 @@ st.title('Movie Recommender System')
 selected_movies = st.selectbox(
     'Movies on your demand',
     movies['title'].values)
-if st.button('recommend'):
-    recommended_movie_names, recommended_movie_posters = recommend(selected_movies)
-    col1, col2, col3, col4, col5 = st.columns(5)
-    with col1:
-        st.text(recommended_movie_names[0])
-        st.image(recommended_movie_posters[0])
-    with col2:
-        st.text(recommended_movie_names[1])
-        st.image(recommended_movie_posters[1])
+try:
+    if st.button('recommend'):
+        recommended_movie_names, recommended_movie_posters = recommend(selected_movies)
+        col1, col2, col3, col4, col5 = st.columns(5)
+        with col1:
+            st.text(recommended_movie_names[0])
+            st.image(recommended_movie_posters[0])
+        with col2:
+            st.text(recommended_movie_names[1])
+            st.image(recommended_movie_posters[1])
 
-    with col3:
-        st.text(recommended_movie_names[2])
-        st.image(recommended_movie_posters[2])
-    with col4:
-        st.text(recommended_movie_names[3])
-        st.image(recommended_movie_posters[3])
-    with col5:
-        st.text(recommended_movie_names[4])
-        st.image(recommended_movie_posters[4])
+        with col3:
+            st.text(recommended_movie_names[2])
+            st.image(recommended_movie_posters[2])
+        with col4:
+            st.text(recommended_movie_names[3])
+            st.image(recommended_movie_posters[3])
+        with col5:
+            st.text(recommended_movie_names[4])
+            st.image(recommended_movie_posters[4])
+except Exception as e:
+    st.subheader("we are working on that try another movie ")
+
